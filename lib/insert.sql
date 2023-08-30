efb049c4eabd18fe47cb9c0f6baace041571de14
Samuel Maiko <sammymaiko721@gmail.com>
2023-08-30 15:54:29 +0300

    Python code done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES ("Mr. Chocolate",13,"M","Brown","Gentle",1);
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES ("Rowdy",13,"M","Brown","Gentle",1);
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES ("Tabitha",13,"F","Brown","Gentle",1);
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES ("Sergeant Brown",13,"M","Brown","Gentle",1);
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES ("Melissa",13,"F","Brown","Gentle",1);
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES ("Grinch",13,"M","Brown","Gentle",1);
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES ("Wendy",13,"F","Brown","Gentle",1);
+INSERT INTO bears (name,age,sex,color, temperament,alive) VALUES (NULL,13,"M","Brown","Gentle",1);
